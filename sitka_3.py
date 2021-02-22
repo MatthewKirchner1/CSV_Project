a958734547f62d00ab684f0f319c31b8131d6d4e
Matthew Kirchner <Matthew_Kirchner1@baylor.edu>
2021-02-22 15:38:40 -0600

    project thru death valley

--- sitka_3.py
+++ sitka_3.py
@@ -1,7 +1,7 @@
 import csv
 from datetime import datetime
 
-open_file = open("sitka_weather_07-2018_simple.csv", "r")
+open_file = open("sitka_weather_2018_simple.csv", "r")
 
 csv_file = csv.reader(open_file, delimiter=",")
 
@@ -15,10 +15,12 @@ for index, column_header in enumerate(header_row):
 
 # Get high temps
 highs = []
+lows = []
 dates = []
 
 for row in csv_file:
     highs.append(int(row[5]))
+    lows.append(int(row[6]))
     converted_date = datetime.strptime(row[2], "%Y-%m-%d")
     dates.append(converted_date)
 
@@ -29,11 +31,21 @@ import matplotlib.pyplot as plt
 fig = plt.figure()
 
 plt.plot(dates, highs, c="red")
-plt.title("Daily high temperatures, July 2018", fontsize=16)
+plt.plot(dates, lows, c="blue")
+
+plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
+
+plt.title("Daily high and low temperatures - 2018", fontsize=16)
 plt.xlabel("Date", fontsize=16)
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis="both", which="major", labelsize=16)
 
 fig.autofmt_xdate()
+
+plt.show()
+
+fig2, a = plt.subplots(2)
+a[0].plot(dates, highs, c="red")
+a[1].plot(dates, lows, c="blue")
 
 plt.show()
